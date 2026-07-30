1ec80944d3641da38796edef362c1e10f9f9d8d9
Gizmuf <gizmuf@gmail.com>
2026-07-30 19:28:09 +0700

    Fix assembly motion availability race

--- frontend/components/ModelViewer.tsx
+++ frontend/components/ModelViewer.tsx
@@ -428,7 +428,6 @@ export default function ModelViewer({
   useEffect(() => {
     setHoveredObjectId(null);
     setSelectedObjectId(null);
-    setMotionAvailable(false);
     setMotionRunning(false);
   }, [src, ghostModelUrl]);
 
