ac9eaee6587423bc5580c656eb1b162adf48bb72
gizmuf <60299799+gizmuf@users.noreply.github.com>
2026-08-20 19:57:22 +0700

    Add baseline frontend security headers

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,6 +14,22 @@ const nextConfig = {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
           },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), geolocation=(), microphone=(self)",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "base-uri 'self'; object-src 'none'; frame-ancestors 'none'",
+          },
         ],
       },
     ];
